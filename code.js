604f4d22c0f0cd79282bc64cf0ecfd6fe8e6a882
skytunnel <32209413+skytunnel@users.noreply.github.com>
2017-09-23 18:44:58 +0100

    Create code.js

---
+++ code.js
@@ -0,0 +1,67 @@
+//Google Drive public file access
+var gDriveFilesUrl = 'https://www.googleapis.com/drive/v2/files/';
+var gDriveApiKey = 'AIzaSyDlD0Io4fOzxFrNz1KQk7XfiMcs8g0HEcY';
+var urlParamFileId = 'gDriveFileId';
+
+//Function to pharse parameter from URL
+function getUrlParam(paraName) {
+    var params = location.search.split('?');
+    params = params[1].split('&');  
+    for (var i = 0; i < params.length; i++) {
+         var temp = params[i].split("=");
+         if (temp[0] == paraName) { 
+             return temp[1]; 
+         }
+    }
+    return;
+}
+
+//function to display message on page
+function displayMsg(msgStr) {
+    var e = document.createElement('pre');
+    e.innerHTML = msgStr;
+    document.body.appendChild(e);
+}
+
+//function to load up html from google drive
+function loadHTML() {
+    //Get user provided file ID for google Drive
+    var gDriveFileId = getUrlParam(urlParamFileId);
+
+    //Exit if not provided
+    if (typeof gDriveFileId != 'string') {
+        displayMsg('url parameter required for ' + urlParamFileId);
+        return;
+    }
+
+    //Compile File Request Url
+    var fileRequestURL = gDriveFilesUrl 
+        + gDriveFileId 
+        + '?key=' + gDriveApiKey 
+        + '&alt=media';
+
+    //Prepare Request
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                //load as html to page
+                document.write(xmlhttp.responseText);
+            } else {
+                //Record error
+                displayMsg('unexpected response:'
+                    + '\n    readyState: ' + xmlhttp.readyState
+                    + '\n    status:     ' + xmlhttp.status
+                    + '\n    statusText: ' + xmlhttp.statusText
+                );
+            }
+        }
+    };
+
+    //Send request
+    xmlhttp.open("GET", fileRequestURL, true);
+    xmlhttp.send();
+}
+
+//load on startup
+loadHTML();
